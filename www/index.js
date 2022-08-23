8b0c8b9a751886c1a80cf72181b5dd5710a19186
Gabe Yuan <gabe@zq.com>
2022-08-23 12:11:53 +0800

    render with js

--- www/index.js
+++ www/index.js
@@ -1,4 +1,15 @@
 // import * as wasm from "hello-wasm-pack";
-import * as wasm from "wasm-game-of-life";
+// import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("gabe");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
